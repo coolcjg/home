ce751553e542f715225bc86c0544a2556da845d7
coolcjg <coolcjg@naver.com>
2025-08-16 00:30:58 +0900

    prod 설정 변경

--- src/main/java/com/cjg/home/config/storage/WebMvcConfig.java
+++ src/main/java/com/cjg/home/config/storage/WebMvcConfig.java
@@ -1,15 +1,20 @@
 package com.cjg.home.config.storage;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Value("${resource.location}")
+    String resourceLocation;
+
     @Override
     public void  addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/storage/**") // --1
-                .addResourceLocations("file:///D://NAS//home_storage/"); //--2
+                .addResourceLocations(resourceLocation); //--2
     }
 
 }
