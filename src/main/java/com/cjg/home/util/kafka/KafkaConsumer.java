eac94d4be20306f171ebd0d24c99ba288f50aa45
coolcjg <coolcjg@naver.com>
2025-08-25 18:23:18 +0900

    kafka consumer 기능 확인

--- src/main/java/com/cjg/home/util/kafka/KafkaConsumer.java
+++ src/main/java/com/cjg/home/util/kafka/KafkaConsumer.java
@@ -1,4 +1,23 @@
 package com.cjg.home.util.kafka;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import lombok.extern.log4j.Log4j2;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Log4j2
 public class KafkaConsumer {
+
+    @KafkaListener(topics="test")
+    public void listener(Object data) {
+        ConsumerRecord consumerRecord = (ConsumerRecord)data;
+        String value = (String)consumerRecord.value();
+
+        JsonObject jo = JsonParser.parseString(value).getAsJsonObject();
+        log.info("param {}", jo);
+
+    }
 }
